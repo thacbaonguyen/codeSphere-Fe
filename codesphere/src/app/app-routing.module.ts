34c90991f028a3a1d3864f954aeba3ea85b41343
thacbaonguyen <ar.thbaonguyen@gmail.com>
2025-02-19 21:35:06 +0700

    route-guard-service

--- codesphere/src/app/app-routing.module.ts
+++ codesphere/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import {DashboardComponent} from "./dashboard/dashboard.component";
+import {RouteGuardService} from "./services/route-guard/route-guard.service";
 
 const routes: Routes = [
   {
@@ -16,15 +17,19 @@ const routes: Routes = [
   {
     path: 'codesphere',
     component: DashboardComponent,  // Thêm component cha
+    canActivate: [RouteGuardService],
+    data: {
+      allowedRoles: ['ADMIN', 'MANAGER']
+    },
     children: [
       {
         path: '',
         redirectTo: '/codesphere/dashboard',  // Sửa đường dẫn redirect đầy đủ
-        pathMatch: 'full'
+        pathMatch: 'full',
       },
       {
         path: 'dashboard',
-        loadChildren: () => import('./dashboard/dashboard.module').then((m) => m.DashboardModule)
+        loadChildren: () => import('./dashboard/dashboard.module').then((m) => m.DashboardModule),
       }
     ]
   },
